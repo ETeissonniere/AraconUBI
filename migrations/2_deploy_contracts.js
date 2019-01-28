2fc4fcd6804b44a9a0eb26421f9502b73866b920
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-28 14:19:50 +0100

    Ganache and migrations

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+const Ubi = artifacts.require("Ubi.sol")
+const UbiToken = artifacts.require("UbiToken.sol")
+
+// Change me as needed
+const endingBlock = 4242424242
+
+module.exports = (deployer) => {
+    return deployer.deploy(UbiToken)
+        .then(() => {
+            return deployer.deploy(Ubi, UbiToken.address, endingBlock)
+        })
+}
